b5a27dda5805f48dcc761823ce9f02eaab8acde9
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-19 10:32:33 +0100

    init repo oxidio/module-factfinder

---
+++ README.md
@@ -0,0 +1,1 @@
+# oxidio/module-factfinder
---
+++ metadata.php
@@ -0,0 +1,16 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+$aModule = [
+    'id'          => 'io/factfinder',
+    'title'       => 'oxidio/module-factfinder',
+    'author'      => 'oxidio',
+    'url'         => 'https://github.com/oxidio/module-factfinder',
+    'extend'      => [],
+    'controllers' => [],
+    'templates'   => [],
+    'events'      => [],
+    'blocks'      => [],
+    'settings'    => []
+];

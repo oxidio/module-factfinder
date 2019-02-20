7806f0a931de2c412ef5dfd024a999473ac79d2f
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-20 23:27:02 +0100

    wip

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -1,1 +1,10 @@
 <?php
+
+call_user_func(function(...$files) {
+    foreach ($files as $file) {
+        if (file_exists($file)) {
+            /** @noinspection PhpIncludeInspection */
+            require_once $file;
+        }
+    }
+}, __DIR__ . '/../vendor/fact-finder/fact-finder-php-library/src/FACTFinder/Loader.php');

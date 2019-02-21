3668c0896235551bcb118603e06dfe71cbf742db
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-02-21 21:23:25 +0100

    wip

---
+++ config/di-ff.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace DI;
+
+use FACTFinder\ {
+    Adapter,
+    Core,
+    Core\Client,
+    Core\Server,
+    Util
+};
+use Oxidio\FF;
+
+return [
+    Util\LoggerInterface::class => create(Util\NullLogger::class),
+
+    Core\ConfigurationInterface::class => function() {
+        return new Core\ManualConfiguration(array_merge(FF\CONFIG, [
+            FF\CONFIG\SERVERADDRESS => getenv('FF_SERVERADDRESS'),
+            FF\CONFIG\CONTEXT       => getenv('FF_CONTEXT'),
+            FF\CONFIG\CHANNEL       => getenv('FF_CHANNEL'),
+            FF\CONFIG\USERNAME      => getenv('FF_USERNAME'),
+            FF\CONFIG\PASSWORD      => getenv('FF_PASSWORD'),
+        ]));
+    },
+
+    Core\AbstractEncodingConverter::class => function(Util\LoggerInterface $logger, Core\ConfigurationInterface $config) {
+        if (extension_loaded('iconv')) {
+            return new Core\IConvEncodingConverter(get_class($logger), $config);
+        }
+        if (function_exists('utf8_encode') && function_exists('utf8_decode')) {
+            return new Core\Utf8EncodingConverter(get_class($logger), $config);
+        }
+        return null;
+    },
+
+    Util\CurlInterface::class => create(Util\Curl::class),
+
+    Client\RequestParser::class => function(
+        Util\LoggerInterface $logger,
+        Core\ConfigurationInterface $config,
+        Core\AbstractEncodingConverter $converter = null
+    ) {
+        return new Client\RequestParser(get_class($logger), $config, $converter);
+    },
+
+    Util\Parameters::class => factory(function(Client\RequestParser $parser) {
+        return $parser->getRequestParameters();
+    }),
+
+    Server\RequestFactoryInterface::class => function(
+        Util\LoggerInterface $logger,
+        Core\ConfigurationInterface $config,
+        Util\Parameters $params,
+        Util\CurlInterface $curl
+    ) {
+        return new Server\MultiCurlRequestFactory(get_class($logger), $config, $params, $curl);
+    },
+
+    Server\Request::class => factory(function(Server\RequestFactoryInterface $factory) {
+        return $factory->getRequest();
+    }),
+
+    Client\UrlBuilder::class => function(
+        Util\LoggerInterface $logger,
+        Core\ConfigurationInterface $config,
+        Client\RequestParser $parser,
+        Core\AbstractEncodingConverter $converter = null
+    ) {
+        return new Client\UrlBuilder(get_class($logger), $config, $parser, $converter);
+    },
+
+    Adapter\Suggest::class => function(
+        Util\LoggerInterface $logger,
+        Core\ConfigurationInterface $config,
+        Server\Request $request,
+        Client\UrlBuilder $builder,
+        Core\AbstractEncodingConverter $converter = null
+    ) {
+        return new Adapter\Suggest(get_class($logger), $config, $request, $builder, $converter);
+    },
+
+    Adapter\Search::class => function(
+        Util\LoggerInterface $logger,
+        Core\ConfigurationInterface $config,
+        Server\Request $request,
+        Client\UrlBuilder $builder,
+        Core\AbstractEncodingConverter $converter = null
+    ) {
+        return new Adapter\Search(get_class($logger), $config, $request, $builder, $converter);
+    },
+];
--- src/autoload-files.php
+++ src/autoload-files.php
@@ -1,10 +1,81 @@
-<?php
+<?php /** @noinspection SpellCheckingInspection */
 
-call_user_func(function(...$files) {
-    foreach ($files as $file) {
-        if (file_exists($file)) {
-            /** @noinspection PhpIncludeInspection */
-            require_once $file;
+namespace {
+    call_user_func(function (...$files) {
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                /** @noinspection PhpIncludeInspection */
+                require_once $file;
+            }
         }
-    }
-}, __DIR__ . '/../vendor/fact-finder/fact-finder-php-library/src/FACTFinder/Loader.php');
+    }, __DIR__ . '/../vendor/fact-finder/fact-finder-php-library/src/FACTFinder/Loader.php');
+}
+
+namespace Oxidio\FF\CONFIG {
+    const SERVERMAPPINGS            = 'serverMappings';
+    const IGNOREDSERVERPARAMETERS   = 'ignoredServerParameters';
+    const WHITELISTSERVERPARAMETERS = 'whitelistServerParameters';
+    const REQUIREDSERVERPARAMETERS  = 'requiredServerParameters';
+    const CLIENTMAPPINGS            = 'clientMappings';
+    const IGNOREDCLIENTPARAMETERS   = 'ignoredClientParameters';
+    const WHITELISTCLIENTPARAMETERS = 'whitelistClientParameters';
+    const REQUIREDCLIENTPARAMETERS  = 'requiredClientParameters';
+    const DEBUG                     = 'debug';
+    const REQUESTPROTOCOL           = 'requestProtocol';
+    const SERVERADDRESS             = 'serverAddress';
+    const PORT                      = 'port';
+    const CONTEXT                   = 'context';
+    const CHANNEL                   = 'channel';
+    const LANGUAGE                  = 'language';
+    const AUTHENTICATIONTYPE        = 'authenticationType';
+    const USERNAME                  = 'userName';
+    const PASSWORD                  = 'password';
+    const AUTHENTICATIONPREFIX      = 'authenticationPrefix';
+    const AUTHENTICATIONPOSTFIX     = 'authenticationPostfix';
+    const DEFAULTCONNECTTIMEOUT     = 'defaultConnectTimeout';
+    const DEFAULTTIMEOUT            = 'defaultTimeout';
+    const SUGGESTCONNECTTIMEOUT     = 'suggestConnectTimeout';
+    const SUGGESTTIMEOUT            = 'suggestTimeout';
+    const TRACKINGCONNECTTIMEOUT    = 'trackingConnectTimeout';
+    const TRACKINGTIMEOUT           = 'trackingTimeout';
+    const IMPORTCONNECTTIMEOUT      = 'importConnectTimeout';
+    const IMPORTTIMEOUT             = 'importTimeout';
+    const PAGECONTENTENCODING       = 'pageContentEncoding';
+    const CLIENTURLENCODING         = 'clientUrlEncoding';
+}
+
+namespace Oxidio\FF {
+    const CONFIG = [
+        CONFIG\SERVERMAPPINGS            => [],
+        CONFIG\IGNOREDSERVERPARAMETERS   => [],
+        CONFIG\WHITELISTSERVERPARAMETERS => [],
+        CONFIG\REQUIREDSERVERPARAMETERS  => [],
+        CONFIG\CLIENTMAPPINGS            => [],
+        CONFIG\IGNOREDCLIENTPARAMETERS   => [],
+        CONFIG\WHITELISTCLIENTPARAMETERS => [],
+        CONFIG\REQUIREDCLIENTPARAMETERS  => [],
+        CONFIG\DEBUG                     => false,
+        CONFIG\REQUESTPROTOCOL           => 'https',
+        CONFIG\SERVERADDRESS             => '',
+        CONFIG\PORT                      => '',
+        CONFIG\CONTEXT                   => '',
+        CONFIG\CHANNEL                   => '',
+        CONFIG\LANGUAGE                  => 'de',
+        CONFIG\AUTHENTICATIONTYPE        => 'advanced',
+        CONFIG\USERNAME                  => '',
+        CONFIG\PASSWORD                  => '',
+        CONFIG\AUTHENTICATIONPREFIX      => 'FACT-FINDER',
+        CONFIG\AUTHENTICATIONPOSTFIX     => 'FACT-FINDER',
+        CONFIG\DEFAULTCONNECTTIMEOUT     => '2',
+        CONFIG\DEFAULTTIMEOUT            => '4',
+        CONFIG\SUGGESTCONNECTTIMEOUT     => '2',
+        CONFIG\SUGGESTTIMEOUT            => '1',
+        CONFIG\TRACKINGCONNECTTIMEOUT    => '2',
+        CONFIG\TRACKINGTIMEOUT           => '1',
+        CONFIG\IMPORTCONNECTTIMEOUT      => '10',
+        CONFIG\IMPORTTIMEOUT             => '360',
+        CONFIG\PAGECONTENTENCODING       => 'UTF-8',
+        CONFIG\CLIENTURLENCODING         => 'UTF-8',
+    ];
+}
+
